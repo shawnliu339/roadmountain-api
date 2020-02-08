8ce191667609446f6c829e766ecbf7821d8696ae
siyuan.liu <sranliu1993@gmail.com>
2020-02-08 11:45:26 +0900

    Delete HTML and add cors config

--- src/main/kotlin/com/roadmountain/sim/configuration/WebConfiguration.kt
+++ src/main/kotlin/com/roadmountain/sim/configuration/WebConfiguration.kt
@@ -1,6 +1,7 @@
 package com.roadmountain.sim.configuration
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -10,5 +11,13 @@ class WebConfiguration : WebMvcConfigurer {
         registry.addResourceHandler("/resources/templates/**")
             .addResourceLocations("/resources/templates")
             .setCachePeriod(0)
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("roadmountain-activate.herokuapp.com")
+            .allowedMethods()
+            .allowCredentials(false)
+            .maxAge(3600)
     }
 }
