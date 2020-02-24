e68a71df443281d0b6878255c68d2a474aeb8ffa
siyuan.liu <sranliu1993@gmail.com>
2020-02-24 22:14:23 +0900

    Deleted expired privacy.

--- src/main/kotlin/com/roadmountain/sim/schedule/ScheduleController.kt
+++ src/main/kotlin/com/roadmountain/sim/schedule/ScheduleController.kt
@@ -2,6 +2,7 @@ package com.roadmountain.sim.schedule
 
 import com.roadmountain.sim.logging.LoggerDelegate
 import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -18,5 +19,12 @@ class ScheduleController(
     fun sendCsvToProvider() {
         logger.info("Start to send CSV to the provider.")
         scheduleService.sendCsvToProvider()
+    }
+
+    @DeleteMapping("/schedules/privacy/30days")
+    @Scheduled(cron = "0 0/15 * * * *")
+    fun deletePrivacySavedMoreThan30Days() {
+        logger.info("Start privacy deleting batch")
+        scheduleService.deletePrivacySavedMoreThan30Days()
     }
 }
--- src/main/kotlin/com/roadmountain/sim/schedule/ScheduleService.kt
+++ src/main/kotlin/com/roadmountain/sim/schedule/ScheduleService.kt
@@ -2,16 +2,25 @@ package com.roadmountain.sim.schedule
 
 import com.roadmountain.sim.gmail.GmailService
 import com.roadmountain.sim.register.CsvService
+import com.roadmountain.sim.repository.RegistrationRepository
+import org.springframework.data.domain.Range
 import org.springframework.stereotype.Service
+import java.time.*
 
 @Service
 class ScheduleService(
+    private val clock: Clock,
     private val csvService: CsvService,
-    private val gmailService: GmailService
+    private val gmailService: GmailService,
+    private val repository: RegistrationRepository
 ) {
     companion object {
         private const val OWNER = "liulu1977@gmail.com"
         private const val PROVIDER = "sranliu1993@gmail.com"
+        private val SERVICE_START_TIME = LocalDateTime.of(
+            LocalDate.parse("2019-03-01"),
+            LocalTime.MIN
+        ).toInstant(ZoneOffset.UTC)
     }
 
     fun sendCsvToProvider() {
@@ -23,5 +32,17 @@ class ScheduleService(
             file = file
         )
         gmailService.sendMessage(content)
+    }
+
+    fun deletePrivacySavedMoreThan30Days() {
+        val thirtyDaysAgo = LocalDate.now(clock).minusDays(30)
+        val moreThan30Days = Range.closed(
+            SERVICE_START_TIME,
+            LocalDateTime.of(thirtyDaysAgo, LocalTime.MAX).toInstant(ZoneOffset.UTC)
+        )
+        val deletedList = repository.findByCreatedBetween(moreThan30Days)
+            .map { it.copy(privacy = null) }
+
+        repository.saveAll(deletedList)
     }
 }
