e11d23680d30316f713c80e5e7f59d9e61131dde
siyuan.liu <sranliu1993@gmail.com>
2020-03-04 10:34:27 +0900

    Added csrf token into cookie.

--- src/main/kotlin/com/roadmountain/sim/config/WebSecurityConfiguration.kt
+++ src/main/kotlin/com/roadmountain/sim/config/WebSecurityConfiguration.kt
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 
 
 @Configuration
@@ -16,7 +17,11 @@ class WebSecurityConfiguration(
     private val passwordEncoder: BCryptPasswordEncoder
 ) : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity) {
-        http.authorizeRequests()
+        http.csrf()
+            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            .and()
+
+            .authorizeRequests()
             .antMatchers("/api/admin/reset")
             .permitAll()
 
